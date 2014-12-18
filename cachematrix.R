066d0e705c29e42131d88cc28117131f210b6a34
m8masha <mm8mary@gmail.com>
2014-12-18 16:02:36 +0400

    add cachematrix.R

--- cachematrix.R
+++ cachematrix.R
@@ -1,15 +1,35 @@
-## Put comments here that give an overall description of what your
-## functions do
+## Caching the Data of Inversed Matrix
 
-## Write a short comment describing this function
+## 1.1 makeCacheMatrix - this function creates a special object that can cache its inverse
 
 makeCacheMatrix <- function(x = matrix()) {
-
+    s <- NULL # s will be my "solved" matrix
+    set <- function(y){ # takes an input matrix
+      x <<- y # saves the input matrix
+      s <<- NULL # reserts the s to NULL (when a new object is generated)
+    }
+   
+    #next three functions will be used by cacheSolve to get data for s:
+     
+    get <- function() {x} # returns the value of original matrix
+    setSolvedMatrix <- function(solve) {s <<- solve} # will be called by cacheSolve() during the first access
+                                                     # and will store the value using super-assignment (i.e. "<<-")
+    getSolvedMatrix <- function() {s} # this will return the cached value to cacheSolve on subsequent accesses
+    list(set = set, get = get, setSolvedMatrix = setSolvedMatrix, getSolvedMatrix = getSolvedMatrix) # a list of internal functions ('methods')
 }
 
-
-## Write a short comment describing this function
-
-cacheSolve <- function(x, ...) {
-        ## Return a matrix that is the inverse of 'x'
+    
+## 1.2 cacheSolve - this function computes the inverse of the special "matrix" returned by makeCacheMatrix above, 
+# if the inverse has already been calculated (and the matrix has not changed), then the cachesolve retrieves the inverse from the cache.
+cacheSolve <- function(x, ...) { # the input x is an object created by makeVector
+        s <- x$getSolvedMatrix() # accesses the object 'x' and gets the value of the mean
+        if(!is.null(s)) { #'if s isn't NULL, then...'
+          
+          message("getting inversed matrix")
+          return(s) # returns the inversed matrix (that has been 'solved' previously)
+        }
+        data <- x$get() # we reach this code if s=NULL. We get the data of original matrix
+        s <- solve(data, ...) # 'solve' (i.e. inverse) the original matrix
+        x$setSolvedMatrix(s) # store the inversed matrix
+        s # return the inversed matrix to the code that called this function
 }
